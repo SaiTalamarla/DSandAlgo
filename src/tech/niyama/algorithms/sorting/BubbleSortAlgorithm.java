944aa979b9a8ec768e6f58ae36bf2013cc07e432
SaiTalamarla <saikishantalamarla@gmail.com>
2020-10-07 11:27:56 +0530

    SortAlgorithm

---
+++ src/tech/niyama/algorithms/sorting/BubbleSortAlgorithm.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package tech.niyama.algorithms.sorting;
+
+import java.util.Arrays;
+
+/**
+ * @author SaiKishan
+ *
+ */
+public class BubbleSortAlgorithm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] data = { -2, 45, 0, 11, -9 };
+		System.out.println(Arrays.toString(sortData(data)));
+	}
+
+	/**
+	 * @param data
+	 * @return
+	 */
+	public static int[] sortData(int[] data) {
+		int arrLen = data.length;
+		// Moving the highest value element to the Last
+		// using swapping
+
+		for (int i = 0; i < arrLen - 1; i++) {
+			for (int j = 0; j < arrLen - i - 1; j++) {
+				int temp;
+				if (data[j] > data[j + 1]) {
+					temp = data[j];
+					data[j] = data[j + 1];
+					data[j + 1] = temp;
+				}
+			}
+
+		}
+		return data;
+	}
+
+}
