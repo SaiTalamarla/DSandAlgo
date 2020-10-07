8bf20d2eb303061266e97ca8134b7cac505a9496
SaiTalamarla <saikishantalamarla@gmail.com>
2020-10-07 12:14:18 +0530

    Optimized Bubble Sort

--- src/tech/niyama/algorithms/sorting/BubbleSortAlgorithm.java
+++ src/tech/niyama/algorithms/sorting/BubbleSortAlgorithm.java
@@ -31,15 +31,18 @@ public class BubbleSortAlgorithm {
 		// using swapping
 
 		for (int i = 0; i < arrLen - 1; i++) {
+			boolean swapped = true;
 			for (int j = 0; j < arrLen - i - 1; j++) {
 				int temp;
 				if (data[j] > data[j + 1]) {
 					temp = data[j];
 					data[j] = data[j + 1];
 					data[j + 1] = temp;
+					swapped = false;
 				}
 			}
-
+			if(swapped = true)
+				break;
 		}
 		return data;
 	}
